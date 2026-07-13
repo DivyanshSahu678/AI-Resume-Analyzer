133bd1c08338230ea43108c50f8fdd47d62da0b1
DivyanshSahu678 <sahudivyansh965@gmail.com>
2026-07-13 23:59:06 +0530

    Implement resume parser

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello")
+from src.resume_parser import extract_text_from_pdf
+
+resume_path = "data/resumes/Divyansh_Sahu_Resume.pdf"
+
+resume_text = extract_text_from_pdf(resume_path)
+
+print("Resume Text:\n")
+print(resume_text)
